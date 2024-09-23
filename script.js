372de328f04c7279f96edce77ca28ef26a552717
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-24 00:08:49 +0300

    JS

---
+++ script.js
@@ -0,0 +1,73 @@
+1. //An Introduction to JavaScript
+
+
+//What is JavaScript?
+
+
+//JavaScript was initially created to “make web pages alive”.
+
+//The programs in this language are called scripts. They can be written right in a web page’s HTML and run automatically as the page loads.
+
+//Scripts are provided and executed as plain text. They don’t need special preparation or compilation to run.
+
+
+//Today, JavaScript can execute not only in the browser, but also on the server, or actually on any device that has a special program called the JavaScript engine.
+
+
+//The browser has an embedded engine sometimes called a “JavaScript virtual machine”.
+
+//Different engines have different “codenames”. For example:
+
+//V8 – in Chrome, Opera and Edge.
+//…There are other codenames like “Chakra” for IE, “JavaScriptCore”, “Nitro” and //“SquirrelFish” for Safari, etc.
+
+
+
+//How do engines work?
+//Engines are complicated. But the basics are easy.
+
+//The engine (embedded if it’s a browser) reads (“parses”) the script.
+//Then it converts (“compiles”) the script to machine code.
+//And then the machine code runs, pretty fast.
+//The engine applies optimizations at each step of the process. It even watches the compiled script as it runs, analyzes the data that flows through it, and further optimizes the machine code based on that knowledge.
+
+
+
+
+//What can in-browser JavaScript do?
+
+
+// in-browser JavaScript is able to:
+
+//Add new HTML to the page, change the existing content, modify styles.
+//React to user actions, run on mouse clicks, pointer movements, key presses.
+//Send requests over the network to remote servers, download and upload files (so-called AJAX and COMET technologies).
+//Get and set cookies, ask questions to the visitor, show messages.
+//Remember the data on the client-side (“local storage”).
+
+
+
+//What makes JavaScript unique?
+//There are at least three great things about JavaScript:
+
+//Full integration with HTML/CSS.
+//Simple things are done simply.
+//Supported by all major browsers and enabled by default.
+//JavaScript is the only browser technology that combines these three things.
+
+//That’s what makes JavaScript unique. That’s why it’s the most widespread tool for creating browser interfaces.
+
+//That said, JavaScript can be used to create servers, mobile applications, etc.
+
+
+
+
+// Summary
+//JavaScript was initially created as a browser-only language, but it is now used in many other environments as well.
+//Today, JavaScript has a unique position as the most widely-adopted browser language, fully integrated with HTML/CSS.
+//There are many languages that get “transpiled” to JavaScript and provide certain features. It is recommended to take a look at them, at least briefly, after mastering JavaScript.
+
+
+
+2.  //IDE
+//The term IDE (Integrated Development Environment)
