9432abdbbc5e0413a3222d5a206840b3c4932e21
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-26 01:26:39 +0300

    completed

--- script.js
+++ script.js
@@ -146,3 +146,169 @@
 
 
 3. //Data types
+
+
+//There are eight basic data types in JavaScript. 
+
+
+3.1 //Number
+
+//The number type represents both integer and floating point numbers.
+let n = 123;
+n = 12.345;
+
+
+//Infinity represents the mathematical Infinity ∞. It is a special value that’s greater than any number.
+
+//We can get it as a result of division by zero:
+alert( 1 / 0 ); // Infinity
+
+//NaN represents a computational error. It is a result of an incorrect or an undefined mathematical operation, for instance:
+
+alert( "not a number" / 2 ); // NaN, such division is erroneous
+//NaN is sticky. Any further mathematical operation on NaN returns NaN:
+
+alert( NaN + 1 ); // NaN
+alert( 3 * NaN ); // NaN
+alert( "not a number" / 2 - 1 ); // NaN
+
+
+//So, if there’s a NaN somewhere in a mathematical expression, it propagates to the whole result (there’s only one exception to that: NaN ** 0 is 1).
+
+
+//A BigInt value is created by appending n to the end of an integer:
+
+// the "n" at the end means it's a BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+
+
+
+3.2 //String
+
+
+//A string in JavaScript must be surrounded by quotes.
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+
+//In JavaScript, there are 3 types of quotes.
+
+//Double quotes: "Hello".
+//Single quotes: 'Hello'.
+//Backticks: `Hello`
+
+
+//Double and single quotes are “simple” quotes. There’s practically no difference between them in JavaScript.
+
+//Backticks are “extended functionality” quotes. They allow us to embed variables and expressions into a string by wrapping them in ${…}
+
+
+let name = "John";
+
+// embed a variable
+alert( `Hello, ${name}!` ); // Hello, John!
+
+// embed an expression
+alert( `the result is ${1 + 2}` ); // the result is 3
+
+
+3.3 //Boolean (logical type)
+
+
+//The boolean type has only two values: true and false.
+
+//This type is commonly used to store yes/no values: true means “yes, correct”, and false means “no, incorrect”.
+
+
+//Boolean values also come as a result of comparisons:
+
+let isGreater = 4 > 1;
+
+alert( isGreater ); // true (the comparison result is "yes")
+
+
+3.4 //The “null” value
+
+
+//The special null value does not belong to any of the types described above.
+
+//It forms a separate type of its own which contains only the null value:
+
+
+//It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+
+
+3.5 //The “undefined” value
+
+//The special value undefined also stands apart. It makes a type of its own, just like null.
+
+//The meaning of undefined is “value is not assigned”.
+
+
+//If a variable is declared, but not assigned, then its value is undefined:
+
+let age;
+
+alert(age); // shows "undefined"
+
+
+3.6 //Objects and Symbols
+
+//The object type is special.
+
+//All other types are called “primitive” because their values can contain only a single thing (be it a string or a number or whatever). In contrast, objects are used to store collections of data and more complex entities.
+
+
+//The symbol type is used to create unique identifiers for objects. 
+
+
+3.7 //The typeof operator
+
+`
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function"  (3)
+
+`
+
+//Definitely, null is not an object. It is a special value with a separate type of its own. The behavior of typeof is wrong here
+
+//There are 8 basic data types in JavaScript.
+
+//Seven primitive data types:
+//number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+//bigint for integer numbers of arbitrary length.
+//string for strings. A string may have zero or more characters, there’s no separate single-character type.
+//boolean for true/false.
+//null for unknown values – a standalone type that has a single value null.
+//undefined for unassigned values – a standalone type that has a single value undefined.
+//symbol for unique identifiers.
+//And one non-primitive data type:
+//object for more complex data structures.
+//The typeof operator allows us to see which type is stored in a variable.
+
+//Usually used as typeof x, but typeof(x) is also possible.
+//Returns a string with the name of the type, like "string".
+//For null returns "object" – this is an error in the language, it’s not actually an object.
+
+
+
+
+
+4. //Interaction: alert, prompt, confirm
