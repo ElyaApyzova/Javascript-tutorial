994caf5b694b49197cc69f6fe1188468037ae816
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-23 23:38:24 +0300

    completed

--- script.js
+++ script.js
@@ -2099,3 +2099,132 @@ Summary
 5.//If we don’t want to do anything in the current iteration and would like to forward to the next one, we can use the continue directive.
 
 6.//break/continue support labels before the loop. A label is the only way for break/continue to escape a nested loop to go to an outer one.
+
+
+Tasks(7)
+
+1. //Last loop value
+
+//What is the last value alerted by this code? Why?
+
+//The answer: 1.
+
+`let i = 3;
+
+while (i) {
+  alert( i-- );
+}`
+
+2. //Which values does the while loop show?
+
+//For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+//Both loops alert the same values, or not?
+
+
+//From 1 to 4
+
+`let i = 0;
+while (++i < 5) alert( i );`
+
+
+//From 1 to 5
+
+`let i = 0;
+while (i++ < 5) alert( i );`
+
+//returns the old value
+
+
+3. //Which values get shown by the "for" loop?
+
+//For each loop write down which values it is going to show. Then compare with the answer.
+
+//Both loops alert same values or not?
+
+//The answer: from 0 to 4 in both cases.
+
+for (let i = 0; i < 5; ++i) alert( i );   //The prefix form:
+
+for (let i = 0; i < 5; i++) alert( i );   //The postfix form:
+
+
+4. //Output even numbers in the loop
+
+//Use the for loop to output even numbers from 2 to 10.
+
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    alert( i );
+  }
+}
+
+//We use the “modulo” operator % to get the remainder and check for the evenness here.
+
+
+5. //Replace "for" with "while"
+
+//Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+let r = 0;
+while (r < 3) {
+  alert( `number ${r}!` );
+  r++;
+}
+
+
+6. //Repeat until the input is correct
+
+//Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+//The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+//Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+
+
+let num;
+
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while (num <= 100 && num);
+
+//The loop do..while repeats while both checks are truthy:
+
+
+7. //Output prime numbers
+
+//An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+//In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
+
+//For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
+
+//Write the code which outputs prime numbers in the interval from 2 to n.
+
+//For n = 10 the result will be 2,3,5,7.
+
+P.S //The code should work for any n, not be hard-tuned for any fixed value.
+
+
+`let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
+
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+
+  alert( i ); // a prime
+}`
+
+
+12. //The "switch" statement
+//A switch statement can replace multiple if checks.
+
+//It gives a more descriptive way to compare a value with multiple variants.
+
