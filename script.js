ec11aaff479538fa5131fd71f2e24cdeaade5593
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-25 20:47:04 +0300

    completed

--- script.js
+++ script.js
@@ -2429,3 +2429,194 @@ switch (a) {
 function showMessage() {
   alert( 'Hello everyone!' );
 }
+
+// The function keyword goes first, then goes the name of the function, then a list of parameters between the parentheses (comma-separated) and finally the code of the function, also named “the function body”, between curly braces.
+
+function name(parameter1, parameter2, ... parameterN) {
+  // body
+ }
+
+
+ function showMessage() {
+  alert( 'Hello everyone!' );
+}
+
+showMessage();
+showMessage();
+
+
+//The call showMessage() executes the code of the function. Here we will see the message two times.
+
+//This example clearly demonstrates one of the main purposes of functions: to avoid code duplication.
+
+//If we ever need to change the message or the way it is shown, it’s enough to modify the code in one place: the function which outputs it.
+
+13.2 //Local variables
+
+//A variable declared inside a function is only visible inside that function.
+
+//For example:
+
+function showMessage() {
+  let message = "Hello, I'm JavaScript!"; // local variable
+
+  alert( message );
+}
+
+showMessage(); // Hello, I'm JavaScript!
+
+alert( message ); // <-- Error! The variable is local to the function
+
+
+13.3 // Outer variables
+
+//A function can access an outer variable as well, for example:
+
+`let userName = 'John';
+
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+showMessage(); // Hello, John`
+
+//The function has full access to the outer variable. It can modify it as well.
+
+//For instance:
+
+`let userName = 'John';
+
+function showMessage() {
+  userName = "Bob"; // (1) changed the outer variable
+
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+alert( userName ); // John before the function call
+
+showMessage();
+
+alert( userName ); // Bob, the value was modified by the function`
+
+//The outer variable is only used if there’s no local one.
+
+
+//If a same-named variable is declared inside the function then it shadows the outer one. For instance, in the code below the function uses the local userName. The outer one is ignored:
+
+`let userName = 'John';
+
+function showMessage() {
+  let userName = "Bob"; // declare a local variable
+
+  let message = 'Hello, ' + userName; // Bob
+  alert(message);
+}
+
+// the function will create and use its own userName
+showMessage();
+
+alert( userName ); // John, unchanged, the function did not access the outer variable`
+
+//Global variables
+
+//Variables declared outside of any function, such as the outer userName in the code above, are called global.
+
+//Global variables are visible from any function (unless shadowed by locals).
+
+
+13.4 // Parameters
+
+//We can pass arbitrary data to functions using parameters.
+
+//In the example below, the function has two parameters: from and text.
+
+function showMessage(from, text) { // parameters: from, text
+  alert(from + ': ' + text);
+}
+
+showMessage('Ann', 'Hello!'); // Ann: Hello! (*)
+showMessage('Ann', "What's up?"); // Ann: What's up? (**)
+
+
+//When the function is called in lines (*) and (**), the given values are copied to local variables from and text. Then the function uses them.
+
+//Here’s one more example: we have a variable from and pass it to the function. Please note: the function changes from, but the change is not seen outside, because a function always gets a copy of the value:
+
+function showMessage(from, text) {
+
+  from = '*' + from + '*'; // make "from" look nicer
+
+  alert( from + ': ' + text );
+}
+
+let from = "Ann";
+
+showMessage(from, "Hello"); // *Ann*: Hello
+
+// the value of "from" is the same, the function modified a local copy
+alert( from ); // Ann
+
+
+//When a value is passed as a function parameter, it’s also called an argument.
+
+//In other words, to put these terms straight:
+
+//A parameter is the variable listed inside the parentheses in the function declaration (it’s a declaration time term).
+//An argument is the value that is passed to the function when it is called (it’s a call time term).
+//We declare functions listing their parameters, then call them passing arguments.
+
+//In the example above, one might say: "the function showMessage is declared with two parameters, then called with two arguments: from and "Hello"".
+
+
+13.5  //Returning a value
+
+//The directive return can be in any place of the function. When the execution reaches it, the function stops, and the value is returned to the calling code (assigned to result above).
+
+//There may be many occurrences of return in a single function. For instance:
+
+`function checkAge(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return confirm('Do you have permission from your parents?');
+  }
+}
+
+let age = prompt('How old are you?', 18);
+
+if ( checkAge(age) ) {
+  alert( 'Access granted' );
+} else {
+  alert( 'Access denied' );
+}`
+
+//It is possible to use return without a value. That causes the function to exit immediately.
+
+
+Summary
+
+1.//A function declaration looks like this:
+
+function name(parameters, delimited, by, comma) {
+  /* code */
+}
+
+2.//Values passed to a function as parameters are copied to its local variables.
+3. //A function may access outer variables. But it works only from inside out. The code outside of the function doesn’t see its local variables.
+4. //A function can return a value. If it doesn’t, then its result is undefined.
+5. //To make the code clean and easy to understand, it’s recommended to use mainly local variables and parameters in the function, not outer variables.
+6.//It is always easier to understand a function which gets parameters, works with them and returns a result than a function which gets no parameters, but modifies outer variables as a side effect.
+
+1. //Function naming:
+
+1. //A name should clearly describe what the function does. When we see a function call in the code, a good name instantly gives us an understanding what it does and returns.
+2. //A function is an action, so function names are usually verbal.
+3. //There exist many well-known function prefixes like create…, show…, get…, check… and so on. Use them to hint what a function does.
+
+
+//Functions are the main building blocks of scripts. Now we’ve covered the basics, so we actually can start creating and using them. But that’s only the beginning of the path. We are going to return to them many times, going more deeply into their advanced features.
+
+Tasks
+
