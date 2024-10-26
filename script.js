9fa0e6452fef8b7c9965e9c7d0fc5b222ee8fc9e
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-26 22:13:30 +0300

    completed

--- script.js
+++ script.js
@@ -2755,3 +2755,182 @@ if (n < 1) {
 
 
 14. // Function expressions
+
+let sayHi = function() {
+  alert( "Hello" );
+};
+
+//Here we can see a variable sayHi getting a value, the new function, created as function() { alert("Hello"); }.
+
+`Why is there a semicolon at the end?
+You might wonder, why do Function Expressions have a semicolon ; at the end, but Function Declarations do not:
+
+function sayHi() {
+  // ...
+}
+
+let sayHi = function() {
+  // ...
+};
+The answer is simple: a Function Expression is created here as function(…) {…} inside the assignment statement: let sayHi = …;. The semicolon ; is recommended at the end of the statement, it’s not a part of the function syntax.
+
+The semicolon would be there for a simpler assignment, such as let sayHi = 5;, and it’s also there for a function assignment.`
+
+
+14.1 //Callback functions
+
+
+function showOk() {
+  alert( "You agreed." );
+}
+
+function showCancel() {
+  alert( "You canceled the execution." );
+}
+
+// usage: functions showOk, showCancel are passed as arguments to ask
+ask("Do you agree?", showOk, showCancel);
+
+
+// The arguments showOk and showCancel of ask are called callback functions or just callbacks.
+
+
+`A function is a value representing an “action”
+Regular values like strings or numbers represent the data.
+
+A function can be perceived as an action.
+
+We can pass it between variables and run when we want.`
+
+
+14.2  //Function Expression vs Function Declaration
+
+//Let’s formulate the key differences between Function Declarations and Expressions.
+
+//First, the syntax: how to differentiate between them in the code.
+
+
+1. //Function Declaration: a function, declared as a separate statement, in the main code flow:
+
+// Function Declaration
+function sum(a, b) {
+  return a + b;
+}
+
+2. //Function Expression: a function, created inside an expression or inside another syntax construct. Here, the function is created on the right side of the “assignment expression” =:
+
+// Function Expression
+let sum = function(a, b) {
+  return a + b;
+};
+
+// The more subtle difference is when a function is created by the JavaScript engine.
+
+//A Function Expression is created when the execution reaches it and is usable only from that moment.
+
+1.//Once the execution flow passes to the right side of the assignment let sum = function…  – here we go, the function is created and can be used (assigned, called, etc. ) from now on.
+
+2.//Function Declarations are different.
+
+//A Function Declaration can be called earlier than it is defined.
+
+//For example, a global Function Declaration is visible in the whole script, no matter where it is.
+
+//That’s due to internal algorithms. When JavaScript prepares to run the script, it first looks for global Function Declarations in it and creates the functions. We can think of it as an “initialization stage”.
+
+//And after all Function Declarations are processed, the code is executed. So it has access to these functions.
+
+//For example, this works:
+
+sayHi("John"); // Hello, John
+
+function sayHi(name) {
+  alert( `Hello, ${name}` );
+}
+
+3. //The Function Declaration sayHi is created when JavaScript is preparing to start the script and is visible everywhere in it.
+
+//…If it were a Function Expression, then it wouldn’t work:
+
+sayHi("John"); // error!
+
+let sayHi = function(name) {  // (*) no magic any more
+  alert( `Hello, ${name}` );
+};
+
+//Function Expressions are created when the execution reaches them. That would happen only in the line (*). Too late.
+
+//Another special feature of Function Declarations is their block scope.
+
+//In strict mode, when a Function Declaration is within a code block, it’s visible everywhere inside that block. But not outside of it.
+
+// That’s because a Function Declaration is only visible inside the code block in which it resides.
+
+//The correct approach would be to use a Function Expression and assign welcome to the variable that is declared outside of if and has the proper visibility.
+
+//This code works as intended:
+
+`let age = prompt("What is your age?", 18);
+
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Hello!");
+  };
+
+} else {
+
+  welcome = function() {
+    alert("Greetings!");
+  };
+
+}
+
+welcome(); // ok now`
+
+//Or we could simplify it even further using a question mark operator ?:
+
+`let age = prompt("What is your age?", 18);
+
+let welcome = (age < 18) ?
+  function() { alert("Hello!"); } :
+  function() { alert("Greetings!"); };
+
+welcome(); // ok now`
+
+
+`When to choose Function Declaration versus Function Expression?
+As a rule of thumb, when we need to declare a function, the first thing to consider is Function Declaration syntax. It gives more freedom in how to organize our code, because we can call such functions before they are declared.
+
+That’s also better for readability, as it’s easier to look up function f(…) {…} in the code than let f = function(…) {…};. Function Declarations are more “eye-catching”.
+
+…But if a Function Declaration does not suit us for some reason, or we need a conditional declaration (we’ve just seen an example), then Function Expression should be used.`
+
+Summary
+
+1.//Functions are values. They can be assigned, copied or declared in any place of the code.
+
+2.//If the function is declared as a separate statement in the main code flow, that’s called a “Function Declaration”.
+
+3.//If the function is created as a part of an expression, it’s called a “Function Expression”. 
+
+4.//Function Declarations are processed before the code block is executed. They are visible everywhere in the block.
+
+5.//Function Expressions are created when the execution flow reaches them.
+
+
+//In most cases when we need to declare a function, a Function Declaration is preferable, because it is visible prior to the declaration itself. That gives us more flexibility in code organization, and is usually more readable.
+
+//So we should use a Function Expression only when a Function Declaration is not fit for the task. 
+
+
+15.  //Arrow functions, the basics
+
+//There’s another very simple and concise syntax for creating functions, that’s often better than Function Expressions.
+
+//It’s called “arrow functions”, because it looks like this:
+
+`let func = (arg1, arg2, ..., argN) => expression;`
+
