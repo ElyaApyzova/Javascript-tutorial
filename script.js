cfee87e72ed1760d1c6569d8a830589c64830116
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-28 22:33:32 +0300

    completed

--- script.js
+++ script.js
@@ -3262,3 +3262,312 @@ function render() {
 
 21. // Automated testing with Mocha
 
+// Automated testing will be used in further tasks, and it’s also widely used in real projects.
+
+21.1 //Why do we need tests?
+
+//When we write a function, we can usually imagine what it should do: which parameters give which results.
+
+//During development, we can check the function by running it and comparing the outcome with the expected one. For instance, we can do it in the console.
+
+//If something is wrong – then we fix the code, run again, check the result – and so on till it works.
+
+//But such manual “re-runs” are imperfect.
+
+21.2 // Behavior Driven Development (BDD)
+//Let’s start with a technique named Behavior Driven Development or, in short, BDD.
+
+//BDD is three things in one: tests AND documentation AND examples.
+
+21.3 // Development of “pow”: the spec
+
+//Let’s say we want to make a function pow(x, n) that raises x to an integer power n. We assume that n≥0.
+
+//That task is just an example: there’s the ** operator in JavaScript that can do that, but here we concentrate on the development flow that can be applied to more complex tasks as well.
+
+//Before creating the code of pow, we can imagine what the function should do and describe it.
+
+//Such description is called a specification or, in short, a spec, and contains descriptions of use cases together with tests for them, like this:
+
+describe("pow", function() {
+
+  it("raises to n-th power", function() {
+    assert.equal(pow(2, 3), 8);
+  });
+
+});
+
+
+21.4 // A spec has three main building blocks that you can see above:
+
+`describe("title", function() { ... })`
+//What functionality we’re describing? In our case we’re describing the function pow. Used to group “workers” – the it blocks.
+
+`it("use case description", function() { ... })`
+//In the title of it we in a human-readable way describe the particular use case, and the second argument is a function that tests it.
+
+`assert.equal(value1, value2)`
+//The code inside it block, if the implementation is correct, should execute without errors.
+
+//Functions assert.* are used to check whether pow works as expected. Right here we’re using one of them – assert.equal, it compares arguments and yields an error if they are not equal. Here it checks that the result of pow(2, 3) equals 8. There are other types of comparisons and checks, that we’ll add later.
+
+
+21.5 //The development flow
+//The flow of development usually looks like this:
+
+1. //An initial spec is written, with tests for the most basic functionality.
+2. //An initial implementation is created.
+3. //To check whether it works, we run the testing framework Mocha (more details soon) that runs the spec. While the functionality is not complete, errors are displayed. We make corrections until everything works.
+4. //Now we have a working initial implementation with tests.
+5. //We add more use cases to the spec, probably not yet supported by the implementations. Tests start to fail.
+6. //Go to 3, update the implementation till tests give no errors.
+7. //Repeat steps 3-6 till the functionality is ready.
+
+
+//So, the development is iterative. We write the spec, implement it, make sure tests pass, then write more tests, make sure they work etc. At the end we have both a working implementation and tests for it.
+
+21.6 //The spec in action
+//Here in the tutorial we’ll be using the following JavaScript libraries for tests:
+
+1. //Mocha – the core framework: it provides common testing functions including describe and it and the main function that runs tests.
+2. //Chai – the library with many assertions. It allows to use a lot of different assertions, for now we need only assert.equal.
+3. //Sinon – a library to spy over functions, emulate built-in functions and more, we’ll need it much later.
+
+//These libraries are suitable for both in-browser and server-side testing. Here we’ll consider the browser variant.
+
+//The full HTML page with these frameworks and pow spec:
+
+`<!DOCTYPE html>
+<html>
+<head>
+  <!-- add mocha css, to show results -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.css">
+  <!-- add mocha framework code -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.js"></script>
+  <script>
+    mocha.setup('bdd'); // minimal setup
+  </script>
+  <!-- add chai -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/chai/3.5.0/chai.js"></script>
+  <script>
+    // chai has a lot of stuff, let's make assert global
+    let assert = chai.assert;
+  </script>
+</head>
+
+<body>
+
+  <script>
+    function pow(x, n) {
+      /* function code is to be written, empty now */
+    }
+  </script>
+
+  <!-- the script with tests (describe, it...) -->
+  <script src="test.js"></script>
+
+  <!-- the element with id="mocha" will contain test results -->
+  <div id="mocha"></div>
+
+  <!-- run tests! -->
+  <script>
+    mocha.run();
+  </script>
+</body>
+
+</html>`
+
+21.7 // Improving the spec
+
+//What we’ve done is definitely a cheat. The function does not work: an attempt to calculate pow(3,4) would give an incorrect result, but tests pass.
+
+//…But the situation is quite typical, it happens in practice. Tests pass, but the function works wrong. Our spec is imperfect. We need to add more use cases to it.
+
+//Let’s add one more test to check that pow(3, 4) = 81.
+
+//We can select one of two ways to organize the test here:
+
+//The first variant – add one more assert into the same it:
+
+describe("pow", function() {
+
+  it("raises to n-th power", function() {
+    assert.equal(pow(2, 3), 8);
+    assert.equal(pow(3, 4), 81);
+  });
+
+});
+
+//The second – make two tests:
+
+describe("pow", function() {
+
+  it("2 raised to power 3 is 8", function() {
+    assert.equal(pow(2, 3), 8);
+  });
+
+  it("3 raised to power 4 is 81", function() {
+    assert.equal(pow(3, 4), 81);
+  });
+
+});
+
+21.8 // Improving the implementation
+
+//Let’s write something more real for tests to pass:
+
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+//To be sure that the function works well, let’s test it for more values. Instead of writing it blocks manually, we can generate them in for:
+
+describe("pow", function() {
+
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} in the power 3 is ${expected}`, function() {
+      assert.equal(pow(x, 3), expected);
+    });
+  }
+
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+
+});
+
+
+21.9 // Nested describe
+//We’re going to add even more tests. But before that let’s note that the helper function makeTest and for should be grouped together. We won’t need makeTest in other tests, it’s needed only in for: their common task is to check how pow raises into the given power.
+
+//Grouping is done with a nested describe:
+
+describe("pow", function() {
+
+  describe("raises x to power 3", function() {
+
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} in the power 3 is ${expected}`, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+
+  });
+
+  // ... more tests to follow here, both describe and it can be added
+});
+
+
+
+`before/after and beforeEach/afterEach
+We can setup before/after functions that execute before/after running tests, and also beforeEach/afterEach functions that execute before/after every it.
+
+For instance:
+
+describe("test", function() {
+
+  before(() => alert("Testing started – before all tests"));
+  after(() => alert("Testing finished – after all tests"));
+
+  beforeEach(() => alert("Before a test – enter a test"));
+  afterEach(() => alert("After a test – exit a test"));
+
+  it('test 1', () => alert(1));
+  it('test 2', () => alert(2));
+
+});
+The running sequence will be:
+
+Testing started – before all tests (before)
+Before a test – enter a test (beforeEach)
+1
+After a test – exit a test   (afterEach)
+Before a test – enter a test (beforeEach)
+2
+After a test – exit a test   (afterEach)
+Testing finished – after all tests (after)
+Open the example in the sandbox.
+Usually, beforeEach/afterEach and before/after are used to perform initialization, zero out counters or do something else between the tests (or test groups).`
+
+
+`Other assertions
+Please note the assertion assert.isNaN: it checks for NaN.
+
+There are other assertions in Chai as well, for instance:
+
+assert.equal(value1, value2) – checks the equality value1 == value2.
+assert.strictEqual(value1, value2) – checks the strict equality value1 === value2.
+assert.notEqual, assert.notStrictEqual – inverse checks to the ones above.
+assert.isTrue(value) – checks that value === true
+assert.isFalse(value) – checks that value === false
+…the full list is in the docs`
+
+Summary
+
+//In BDD, the spec goes first, followed by implementation. At the end we have both the spec and the code.
+
+//The spec can be used in three ways:
+
+1. //As Tests – they guarantee that the code works correctly.
+2. //As Docs – the titles of describe and it tell what the function does.
+3. //As Examples – the tests are actually working examples showing how a function can be used.
+
+//With the spec, we can safely improve, change, even rewrite the function from scratch and make sure it still works right.
+
+//That’s especially important in large projects when a function is used in many places. When we change such a function, there’s just no way to manually check if every place that uses it still works right.
+
+//Without tests, people have two ways:
+
+//To perform the change, no matter what. And then our users meet bugs, as we probably fail to check something manually.
+//Or, if the punishment for errors is harsh, as there are no tests, people become afraid to modify such functions, and then the code becomes outdated, no one wants to get into it. Not good for development.
+
+
+Tasks
+
+//What's wrong in the test?
+
+//What’s wrong in the test of pow below?
+
+it("Raises x to the power n", function() {
+  let x = 5;
+
+  let result = x;
+  assert.equal(pow(x, 1), result);
+
+  result *= x;
+  assert.equal(pow(x, 2), result);
+
+  result *= x;
+  assert.equal(pow(x, 3), result);
+});
+
+
+solution 
+
+describe("Raises x to power n", function() {
+  it("5 in the power of 1 equals 5", function() {
+    assert.equal(pow(5, 1), 5);
+  });
+
+  it("5 in the power of 2 equals 25", function() {
+    assert.equal(pow(5, 2), 25);
+  });
+
+  it("5 in the power of 3 equals 125", function() {
+    assert.equal(pow(5, 3), 125);
+  });
+});
+
+22. // Polyfills and transpilers
